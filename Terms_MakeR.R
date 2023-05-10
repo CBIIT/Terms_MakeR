d2ebe4cd1dc82c1558bb0f1d4aed1229ad3a0c7e
svburke <42675174+svburke@users.noreply.github.com>
2023-05-10 15:10:13 -0500

    Update Terms_MakeR.R

--- Terms_MakeR.R
+++ Terms_MakeR.R
@@ -57,7 +57,7 @@ option_list = list(
 )
 
 #create list of options and values for file input
-opt_parser = OptionParser(option_list=option_list, description = "\nTerms_MakeR.R v2.0.1")
+opt_parser = OptionParser(option_list=option_list, description = "\nTerms_MakeR.R v2.0.2")
 opt = parse_args(opt_parser)
 
 #If no options are presented, return --help, stop and print the following message.
